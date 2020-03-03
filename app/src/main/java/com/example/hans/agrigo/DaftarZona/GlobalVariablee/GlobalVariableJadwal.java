c0357d9a401e6993746bb9b7e3958c9a57d58ae2
Asep Trisna Setiawan <aseptrisna1995@gmail.com>
2020-03-03 21:28:25 +0700

    fafafaf

---
+++ app/src/main/java/com/example/hans/agrigo/DaftarZona/GlobalVariablee/GlobalVariableJadwal.java
@@ -0,0 +1,4 @@
+package com.example.hans.agrigo.DaftarZona.GlobalVariablee;
+
+public class GlobalVariableJadwal {
+}
---
+++ app/src/main/java/com/example/hans/agrigo/MenuJadwal/AturJadwalSiram.java
@@ -0,0 +1,14 @@
+package com.example.hans.agrigo.MenuJadwal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AturJadwalSiram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atur_jadwal_siram);
+    }
+}
